1e59e4ac419f2959d1e1872f29cd05c28bb9bfc1
Sonali Jatav <sonalijatav972@gmail.com>
2025-07-09 11:46:49 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 
 export default defineConfig({
+  base: './', // very important for routing on Vercel
+  build: {
+    outDir: 'dist'
+  },
   plugins: [react()],
-})
+});
