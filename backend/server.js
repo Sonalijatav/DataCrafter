20d8f3160bc15cfdd57bde2b66282b918d55d50c
Sonali Jatav <sonalijatav972@gmail.com>
2025-07-09 12:55:23 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,25 @@ const User = require('./models/User');
 const app = express();
 // app.use(cors());
 
-app.use(cors({
-  origin: "*"
-}));
-
-
 // app.use(cors({
-//   origin: "https://data-crafter.vercel.app/", // Vercel frontend URL
-//   credentials: true,
+//   origin: "*", // or add your Vercel frontend URL here for security
+//   credentials: true
 // }));
+
+app.use(cors({
+  origin: "https://data-crafter.vercel.app/", // Vercel frontend URL
+  credentials: true,
+  
+}));
 
 // app.use(express.json());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+app.get("/", (req, res) => {
+  res.send("🚀 DataCrafter backend is live and connected!");
+});
 
 
 
